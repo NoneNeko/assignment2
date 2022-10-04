57d378c04379700d6bbadce928af197aa94045f1
Dai Dung Lam <lamdaidung10c5@gmail.com>
2022-10-04 14:25:17 -0400

    initial commit

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/departments", (req,res) =>{
 
 app.get("/managers", (req,res) =>{
     dataService.getManagers().then((data) =>{
-       const manager = data;
+       const manager = req.data;
        let resText = "<br>";
     resText = JSON.stringify(manager) + "<br>";
     res.send(manager);
